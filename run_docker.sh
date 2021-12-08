fd170241ff644838a32acbd525b125f13a0c23b9
Steve <vgan@users.noreply.github.com>
2021-12-08 15:22:38 -0600

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -3,9 +3,9 @@ HOST_MODELS="$HOME/vqgan/models"
 HOST_OUTPUT="$HOME/vqgan/outputs"
 HOST_PORT=8888
 #HOST_PORT=8081
-sudo docker run --name vqgan_clip --gpus all --rm -it -p $HOST_PORT:8888 \
+sudo docker run --name vqganclip --gpus all --rm -it -p $HOST_PORT:8888 \
         -v "$(pwd):/tf/src" \
         -v "$HOST_MODELS:/tf/models" \
         -v "$HOST_OUTPUT:/tf/outputs" \
-        sborquez/vgan:latest\
+        vgan/vqganclip:latest\
         bash
